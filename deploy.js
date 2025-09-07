be0d2afd14328661d7b0fdf9464888ddbb951733
SatinAu-Zelynn <mifanz090820@outlook.com>
2025-09-07 12:54:27 +0800

    update

--- deploy.js
+++ deploy.js
@@ -3,8 +3,7 @@ const ghpages = require('gh-pages');
 ghpages.publish('build', {
   branch: 'gh-pages',
   dotfiles: true,
-  history: false,   // 不保留历史记录，避免 ENAMETOOLONG
-  // add: true,     // 如果你想保留历史记录，但允许覆盖文件，就用这个
+  history: false   // 🚀 关键：跳过 git rm
 }, function (err) {
   if (err) {
     console.error('部署失败:', err);
