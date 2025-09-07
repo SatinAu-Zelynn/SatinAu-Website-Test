4eb4ce240d7211e62865a5d5512aad7d0118f453
SatinAu-Zelynn <mifanz090820@outlook.com>
2025-09-07 12:52:42 +0800

    update

--- deploy.js
+++ deploy.js
@@ -3,7 +3,8 @@ const ghpages = require('gh-pages');
 ghpages.publish('build', {
   branch: 'gh-pages',
   dotfiles: true,
-  history: false   // 关键：避免 spawn ENAMETOOLONG
+  history: false,   // 不保留历史记录，避免 ENAMETOOLONG
+  // add: true,     // 如果你想保留历史记录，但允许覆盖文件，就用这个
 }, function (err) {
   if (err) {
     console.error('部署失败:', err);
